c62578db230276db32e8a67f55a816e63e0eca15
umerhasan2010 <umerhasan2010@outlook.com>
2020-03-27 16:22:34 +0500

    Adding again

--- Dynamic Form Builder using .NET CORE WEB API and Angular/ViewModel/FormItemWithOptions.cs
+++ Dynamic Form Builder using .NET CORE WEB API and Angular/ViewModel/FormItemWithOptions.cs
@@ -9,6 +9,7 @@ namespace Dynamic_Form_Builder_using_.NET_CORE_WEB_API_and_Angular.ViewModel
     public class FormItemWithOptions
     {
         public TblFormItem formItem { get; set; }
+
         public TblOption options { get; set; }
 
     }
